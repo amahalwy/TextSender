c9b180209fef1248d7e4e653d6ca54fdf0369ccf
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-02-23 21:33:02 -0500

    Add slack API to get channel messages on successful sends

--- pages/api/slack/SendSlackMsg.js
+++ pages/api/slack/SendSlackMsg.js
@@ -1,20 +1,18 @@
 import { WebClient, LogLevel } from "@slack/web-api";
 
-const client = new WebClient(
-  "xoxb-1783110254821-1779432119174-yiM8UVos8zVqP9rMpK8pL8tJ"
-);
+const client = new WebClient(process.env.SLACK_API_TOKEN);
 
 export async function publishMessage(text) {
   try {
-    const result = await client.chat.postMessage({
-      token: "xoxb-1783110254821-1779432119174-yiM8UVos8zVqP9rMpK8pL8tJ",
-      channel: "C01P45V5Y8J",
+    await client.chat.postMessage({
+      token: process.env.SLACK_API_TOKEN,
+      channel: process.env.SLACK_CHANNEL_ID,
       text: text,
     });
   } catch (error) {
-    const result = await client.chat.postMessage({
-      token: "xoxb-1783110254821-1779432119174-yiM8UVos8zVqP9rMpK8pL8tJ",
-      channel: "C01P45V5Y8J",
+    await client.chat.postMessage({
+      token: process.env.SLACK_API_TOKEN,
+      channel: process.env.SLACK_CHANNEL_ID,
       text: error,
     });
   }
