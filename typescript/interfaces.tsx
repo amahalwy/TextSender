3e9c92440bfb7ec416cd88915586d3a9fee452aa
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-25 14:32:43 -0700

    Added new interface for receivers of texts

--- typescript/interfaces.tsx
+++ typescript/interfaces.tsx
@@ -1,26 +1,23 @@
 import { FormApi } from "final-form";
-
-export interface Values {
-  numbers: string[];
+export interface IValues {
+  numbers: string;
   accountSid?: string | undefined;
   apiKey?: string | undefined;
   apiSecret?: string | undefined;
   message?: string | undefined;
   from?: string | undefined;
 }
-
 export interface IBottomSection {
   submitting: boolean;
   pristine: boolean;
-  form: FormApi<Values, Partial<Values>>;
+  form: FormApi<IValues, Partial<IValues>>;
   invalid: boolean;
   numbers: any[];
   setShowBottom: (value: boolean) => void;
   setShowTwilioSection: (value: boolean) => void;
 }
-
 export interface IFormSection {
-  values: Values;
+  values: IValues;
   submitting: boolean;
   pristine: boolean;
   invalid: boolean;
@@ -29,4 +26,24 @@ export interface IFormSection {
   setNumbers: (value: string[]) => void;
   setShowTwilioSection: (value: boolean) => void;
   setShowBottom: (value: boolean) => void;
+}
+
+export interface INumber {
+  accountSid: string;
+  apiKey: string;
+  apiSecret: string;
+  message: string;
+  from: string;
+  to: string;
+  timeToSend: number;
+}
+
+export interface IReceiver {
+  to: string;
+  timeToSend: number;
+  accountSid: string | undefined;
+  apiKey: string | undefined;
+  apiSecret: string | undefined;
+  message: string | undefined;
+  from: string | undefined;
 }
