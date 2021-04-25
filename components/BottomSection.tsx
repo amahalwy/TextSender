688790f098960c4792176cfad5146cc9eb26bb0c
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-25 14:25:00 -0700

    Changed components to be implicit return

--- components/BottomSection.tsx
+++ components/BottomSection.tsx
@@ -22,92 +22,85 @@ const BottomSection: React.FC<IBottomSection> = ({
   submitting,
   setShowBottom,
   setShowTwilioSection,
-}) => {
-  return (
-    <Box>
-      <Box mb="20px">
-        <Text color="rgb(0,145,0)">
-          Success! Select your twilio number and send out your awesome messages
-          🙌🏻
-        </Text>
-      </Box>
-      <Field
-        name="from"
-        validate={validateRequired}
-        render={({ input, meta }) => (
-          <FormControl isInvalid={meta.touched && meta.error}>
-            <FormLabel htmlFor="from">Twilio phone number</FormLabel>
-            <Select {...input} id="from" placeholder="Phone Number">
-              {numbers.map((number) => (
-                <option key={number} value={number}>
-                  {number}
-                </option>
-              ))}
-            </Select>
-            {meta.touched && meta.error && (
-              <FormErrorMessage>{meta.error}</FormErrorMessage>
-            )}
-          </FormControl>
-        )}
-      />
-
-      <Field name="message" validate={validateRequired}>
-        {({ input, meta }) => (
-          <FormControl isInvalid={meta.touched && meta.error}>
-            <FormLabel mt="10px" htmlFor="message">
-              Message
-            </FormLabel>
-            <Textarea {...input} id="message" placeholder="Type here..." />
-            {meta.touched && meta.error && (
-              <FormErrorMessage>{meta.error}</FormErrorMessage>
-            )}
-          </FormControl>
-        )}
-      </Field>
-
-      <Field
-        name="numbers"
-        validate={(value) =>
-          validateRequired(value) || validatePhoneNumbers(value)
-        }
-        render={({ input, meta }) => (
-          <FormControl isInvalid={meta.touched && meta.error}>
-            <FormLabel htmlFor="phoneNumbers">Phone numbers</FormLabel>
-            <Textarea
-              {...input}
-              id="phoneNumbers"
-              placeholder="Phone numbers"
-            />
-            {meta.touched && meta.error && (
-              <FormErrorMessage>{meta.error}</FormErrorMessage>
-            )}
-          </FormControl>
-        )}
-      />
-
-      <Stack direction="row" mt="24px">
-        <Button
-          type="submit"
-          disabled={submitting || pristine || invalid}
-          colorScheme="teal"
-        >
-          Send
-        </Button>
-
-        <Button
-          type="reset"
-          onClick={() => {
-            setShowBottom(false);
-            setShowTwilioSection(true);
-            form.reset();
-          }}
-          disabled={submitting || pristine}
-        >
-          Reset form
-        </Button>
-      </Stack>
+}) => (
+  <Box>
+    <Box mb="20px">
+      <Text color="rgb(0,145,0)">
+        Success! Select your twilio number and send out your awesome messages 🙌🏻
+      </Text>
     </Box>
-  );
-};
+    <Field
+      name="from"
+      validate={validateRequired}
+      render={({ input, meta }) => (
+        <FormControl isInvalid={meta.touched && meta.error}>
+          <FormLabel htmlFor="from">Twilio phone number</FormLabel>
+          <Select {...input} id="from" placeholder="Phone Number">
+            {numbers.map((number) => (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            ))}
+          </Select>
+          {meta.touched && meta.error && (
+            <FormErrorMessage>{meta.error}</FormErrorMessage>
+          )}
+        </FormControl>
+      )}
+    />
+
+    <Field name="message" validate={validateRequired}>
+      {({ input, meta }) => (
+        <FormControl isInvalid={meta.touched && meta.error}>
+          <FormLabel mt="10px" htmlFor="message">
+            Message
+          </FormLabel>
+          <Textarea {...input} id="message" placeholder="Type here..." />
+          {meta.touched && meta.error && (
+            <FormErrorMessage>{meta.error}</FormErrorMessage>
+          )}
+        </FormControl>
+      )}
+    </Field>
+
+    <Field
+      name="numbers"
+      validate={(value) =>
+        validateRequired(value) || validatePhoneNumbers(value)
+      }
+      render={({ input, meta }) => (
+        <FormControl isInvalid={meta.touched && meta.error}>
+          <FormLabel htmlFor="phoneNumbers">Phone numbers</FormLabel>
+          <Textarea {...input} id="phoneNumbers" placeholder="Phone numbers" />
+          {meta.touched && meta.error && (
+            <FormErrorMessage>{meta.error}</FormErrorMessage>
+          )}
+        </FormControl>
+      )}
+    />
+
+    <Stack direction="row" mt="24px">
+      <Button
+        type="submit"
+        disabled={submitting || pristine || invalid}
+        colorScheme="teal"
+      >
+        Send
+      </Button>
+
+      <Button
+        type="reset"
+        onClick={() => {
+          setShowBottom(false);
+          setShowTwilioSection(true);
+          form.reset();
+        }}
+        disabled={submitting || pristine}
+      >
+        Reset form
+      </Button>
+    </Stack>
+  </Box>
+);
 
 export default BottomSection;
--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,29 +2,27 @@ import React from "react";
 import Head from "next/head";
 import endpoints from "../config/endpoints";
 
-const GoogleAnalytics: React.FC = () => {
-  return (
-    <>
-      {process.env.NODE_ENV === "production" && process.browser && (
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${endpoints.GAid}`}
-          ></script>
-          <script
-            async
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
+const GoogleAnalytics: React.FC = () => (
+  <>
+    {process.env.NODE_ENV === "production" && process.browser && (
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${endpoints.GAid}`}
+        ></script>
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag("js", new Date());
 
                 gtag("config", "${endpoints.GAid}");`,
-            }}
-          />
-        </Head>
-      )}
-    </>
-  );
-};
+          }}
+        />
+      </Head>
+    )}
+  </>
+);
 
 export default GoogleAnalytics;
