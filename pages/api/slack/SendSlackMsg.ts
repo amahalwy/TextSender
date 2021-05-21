9308467105b3a576a90c47e52ba0caca6d01e866
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-21 10:38:16 -0700

    Change for Slack message to display how many numbers will be sent out to

--- pages/api/slack/SendSlackMsg.ts
+++ pages/api/slack/SendSlackMsg.ts
@@ -3,12 +3,12 @@ import endpoints from "../../../config/endpoints";
 
 const client = new WebClient(endpoints.SlackToken);
 
-export default async () => {
+export default async (numbers: number) => {
   try {
     await client.chat.postMessage({
       token: endpoints.SlackToken,
       channel: endpoints.SlackChannel,
-      text: "New Request. Check out Analytics!!",
+      text: `New Request - Check out Analytics! Sending ${numbers} messages.`,
     });
   } catch (error) {
     await client.chat.postMessage({
